4b2159b4ace27810725a1775acfbec54e05b01d2
Hacer <hacerkilic194@gmail.com>
2023-10-19 22:20:30 +0300

    card

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
-     
-        <Routes>
-          <Route path="/" element={<AllMeetupsPage />} />
-          <Route path="/new-meetup" element={<NewMeetupsPage />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-        </Routes>
-
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
