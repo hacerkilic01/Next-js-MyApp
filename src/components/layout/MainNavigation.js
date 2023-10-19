7873f855081e441e4b59841dd11fe5012e77daef
Hacer <hacerkilic194@gmail.com>
2023-10-19 19:52:17 +0300

    MainNavigation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,25 +1,25 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
-function MainNavigation(){
-    return  (
+function MainNavigation() {
+  return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>  
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
-            <li>
-                <Link to='/'>All Meetups</Link>
-            </li>
-            <li>
-                <Link to='/favorites'>Favorites</Link>
-            </li>
-            <li>
-                <Link to='/new-meetup'>Add new up</Link>
-            </li>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add new up</Link>
+          </li>
         </ul>
       </nav>
     </header>
-    );
+  );
 }
 export default MainNavigation;
