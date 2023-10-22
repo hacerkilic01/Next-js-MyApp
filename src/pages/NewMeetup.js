3e9173e708ebcca192aea6195ac3f6226c62a6d8
Hacer <hacerkilic194@gmail.com>
2023-10-22 14:48:44 +0300

    useState

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,7 @@
+import {useHistory} from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
+  const history =useHistory;
   function addMeetupHandler(meetupData) {
     fetch(
       'https://react-getting-started-6f1d7-default-rtdb.firebaseio.com/meetups.json',
@@ -11,7 +13,11 @@ function NewMeetupPage() {
           'Content-Type': 'application/json'
         }
       }
-    );
+    ).then(()=> {
+      history.replace('/');
+    });
+    
+    
   }
   return (
     <section>
